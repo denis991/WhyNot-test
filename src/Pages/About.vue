7d307cb00ef3748bfb3c21f101a230bfd558f2ce
Denis Gutsulak <d.99113@gmail.com>
2023-06-30 17:49:20 +0300

    +scss

--- src/Pages/About.vue
+++ src/Pages/About.vue
@@ -7,5 +7,12 @@
 </template>
 
 <style scoped lang="scss">
-
+div{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  color: rgb(0, 255, 55);
+}
 </style>
--- src/Pages/index.vue
+++ src/Pages/index.vue
@@ -4,4 +4,13 @@
 	<div>index</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+div{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  color: rgb(0, 255, 55);
+}
+</style>
